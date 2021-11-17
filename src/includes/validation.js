5354afab938edc23634eca777dbf251276cd5f59
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-11-17 12:54:04 +0000

    added extra rulesets for form validation to prevent unessecary data being used

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -2,14 +2,21 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 /* eslint-disable linebreak-style */
-import { Form as VeeForm, Field as VeeField, defineRule } from 'vee-validate';
-import { required } from '@vee-validate/rules';
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate';
+import { required, min, max, alpha_spaces as alphaSpaces, alpha_spaces, email, min_value as minVal, max_value as maxVal } from '@vee-validate/rules';
 
 export default {
 	install(app) {
 		app.component('VeeForm', VeeForm);
 		app.component('VeeField', VeeField);
+		app.component('ErrorMessage', ErrorMessage);
 
 		defineRule('required', required);
+		defineRule('min', min);
+		defineRule('max', max);
+		defineRule('alpha_spaces', alpha_spaces);
+		defineRule('email', email);
+		defineRule('min_value', minVal);
+		defineRule('max_value', maxVal);
 	},
 };
