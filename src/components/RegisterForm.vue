a8be65af793fb0a179049b867af0fc585dbe898c
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-11-28 18:22:09 +0000

    added firebase as a backend method to store user validation account data

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,6 +1,6 @@
 <template>
    <!-- Registration Form -->
-		  <div class="text-white text-center font-bold p-5 mb-4" v-if="reg_show_alert" :class="reg_alert_varient">
+		  <div class="text-white text-center font-bold p-5 mb-4" v-if="reg_show_alert" :class="reg_alert_variant">
 			  {{ reg_alert_msg }}
 		  </div>
           <vee-form :validation-schema="schema" @submit="register" :initial-values="userData">
@@ -83,6 +83,8 @@
 </template>
 
 <script>
+import {auth} from '@/includes/firebase';
+
 export default {
 	name: 'RegisterForm',
 	data(){
@@ -99,22 +101,34 @@ export default {
 	  userData: {
 		country: 'USA',
 	  },
-	  login_in_submission: false,
-	  login_show_alert: false,
-	  login_alert_variant: 'bg-blue-500',
-	  login_alert_msg: 'Please wait! We are logging you in',
+	  reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: 'bg-blue-500',
+      reg_alert_msg: 'Please wait! Your account is being created.',
 	};
 	},
 	methods: {
-	register(values){
+	async register(values) {
 		this.reg_show_alert = true;
 		this.reg_in_submission = true;
 		this.reg_alert_variant = 'bg-blue-500';
 		this.reg_alert_msg = 'Please wait! Your account is being created.';
 
+	let userCred = null;
+	try{
+		userCred = await auth.createUserWithEmailAndPassword(
+			values.email, values.password,
+		);
+	} catch(error) {
+		this.reg_in_submission = false;
+		this.reg_alert_variant = 'bg-red-500';
+		this.reg_alert_msg = 'An unexpected error occured. Please try again later';
+		return;
+	}
+
 		this.reg_alert_variant = 'bg-green-500';
 		this.reg_alert_msg = 'Success! Your account has been created';
-		console.log(values);
+		console.log(userCred);
 	},
 	},
 };
---
+++ src/includes/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+
+	apiKey: 'AIzaSyC7-IEy7qqg97HhpF9-ettNXDzozfHt4bg',
+	authDomain: 'music-80470.firebaseapp.com',
+	projectId: 'music-80470',
+	storageBucket: 'music-80470.appspot.com',
+	appId: '1:143333674081:web:ff2de5a9ee308758d8ae20',
+	measurementId: 'G-SKPS2W99M1',
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export {
+	auth,
+	db,
+};
