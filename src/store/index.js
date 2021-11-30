e876bcc28894c306ce3de4c198dd764fb6c0418a
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-11-30 12:42:40 +0000

    added functionality to help the user log in after signup

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-    authModalShow: false,
-  },
-  mutations: {
-    toggleAuthModal: (state) => {
-      state.authModalShow = !state.authModalShow;
-    },
-  },
-  getters: {
-    // authModalShow: (state) => state.authModalShow,
-  },
+	state: {
+		authModalShow: false,
+		userLoggedIn: false,
+	},
+	mutations: {
+		toggleAuthModal: (state) => {
+			state.authModalShow = !state.authModalShow;
+		},
+		toggleAuth(state) {
+			state.userLoggedIn = !state.userLoggedIn;
+		}
+	},
+	getters: {
+		// authModalShow: (state) => state.authModalShow,
+	},
 });
