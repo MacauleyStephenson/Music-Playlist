095ee245a312933ede1e7d4d870c630c6fd43b8f
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-12-05 12:47:23 +0000

    refactored register form to utilise actions and for redability

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -83,7 +83,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
+
 
 export default {
 	name: 'RegisterForm',
@@ -114,36 +114,17 @@ export default {
 		this.reg_alert_variant = 'bg-blue-500';
 		this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-	let userCred = null;
 	try{
-		userCred = await auth.createUserWithEmailAndPassword(
-			values.email, values.password,
-		);
+	  await this.$store.dispatch('register', values);
 	} catch(error) {
 		this.reg_in_submission = false;
 		this.reg_alert_variant = 'bg-red-500';
 		this.reg_alert_msg = 'Sorry, but this email address is already in use';
 		return;
 	}
-	try{
-		await usersCollection.add({
-			name: values.name,
-			email: values.email,
-			age: values.age,
-			country: values.country
-		});
-	}	catch (error){
-		this.reg_in_submission = false;
-		this.reg_alert_variant = 'bg-red-500';
-		this.reg_alert_msg = 'An unexpected error occured. Please try again later';
-		return;
-	}
-
-		this.$store.commit('toggleAuth');
 
 		this.reg_alert_variant = 'bg-green-500';
 		this.reg_alert_msg = 'Success! Your account has been created';
-		console.log(userCred);
 	},
 	},
 };
