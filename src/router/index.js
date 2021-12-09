8ddc527df3d0bcfe658b6c2adf47b68edb9abf4d
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-12-09 17:42:35 +0000

    implimenting routing and naming schemes/aliases

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+	{
+		name: 'home',
+		path: '/',
+		component: Home,
+	},
+	{
+		name: 'about',
+		path: '/about',
+		component: About,
+	},
+	{
+		name: 'manage',
+		path: '/manage',
+		component: Manage,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHashHistory(process.env.BASE_URL),
+	routes,
+	linkActiveClass: 'text-yellow-500',
 });
 
 export default router;
