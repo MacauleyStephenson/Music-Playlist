fc62abd3dce7638be19208cf04cfa905f8a73c51
MacauleyStephenson <MacauleyStephenson@mail.com>
2021-12-21 18:58:37 +0000

    added message/vue fuctionality to allow the user to edit songs

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -10,6 +10,9 @@
                 </button>
               </div>
               <div v-show="showForm">
+				<div class="text-white text-center font-bold p-4 mb-4" v-if="show_alert" :class="alert_varient">
+				  {{ alert_message }}
+			  </div>
                 <vee-form :validation-schema="schema" :initial-values="song" @submit="edit">
                   <div class="mb-3">
                     <label class="inline-block mb-2">Song Title</label>
@@ -27,10 +30,10 @@
                       placeholder="Enter Genre" />
 					  <ErrorMessage class="text-red-600" name="genre" />
                   </div>
-                  <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
+                  <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600" :disabled="in_submission">
                     Submit
                   </button>
-                  <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
+                  <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600" :disabled="in_submission" @click.prevent="showForm = false">
                     Go Back
                   </button>
                 </vee-form>
@@ -39,6 +42,8 @@
 </template>
 
 <script>
+import { songsCollection } from '@/includes/firebase';
+
 export default {
 	name: 'CompositionItem',
 	props: {
@@ -46,6 +51,14 @@ export default {
 			type: Object,
 			required: true,
 		},
+		updateSong: {
+			type: Function,
+			required: true,
+		},
+		index: {
+			type: Number,
+			required: true,
+		}
 	},
 	data(){
 		return {
@@ -54,11 +67,32 @@ export default {
 				modified_name: 'required',
 				genre: 'alpha_spaces',
 			},
+			in_submission: false,
+			show_alert: false,
+			alert_varient: 'bg-blue-500',
+			alert_message: 'Please wait, the song information is being updated',
 		};
 	},
 	methods: {
-		edit() {
-		console.log("Song changed");
+		async edit(values) {
+		this.in_submission = true;
+		this.show_alert = true;
+		this.alert_varient = 'bg-blue-500';
+		this.alert_message = 'Please wait, the song information is being updated';
+		try{
+			await songsCollection.doc(this.song.docID).update(values);
+		}catch (error){
+			this.in_submission = false;
+			this.alert_varient = 'bg-red-500';
+			this.alert_message = 'Something went wrong! Please try again later';
+			return;
+		}
+
+		this.updateSong(this.index, values)
+
+		this.in_submission = false;
+		this.alert_varient = 'bg-green-500';
+		this.alert_message = 'Success!';
 		},
 	},
 };
