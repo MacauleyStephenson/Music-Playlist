949a017162414d11e8db6488aee3f4ea23e98e9e
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-01-21 16:24:20 +0000

    implimenting persistance for firebase to allow functionality offline

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC7-IEy7qqg97HhpF9-ettNXDzozfHt4bg',
-  authDomain: 'music-80470.firebaseapp.com',
-  projectId: 'music-80470',
-  storageBucket: 'music-80470.appspot.com',
-  appId: '1:143333674081:web:ff2de5a9ee308758d8ae20',
-  measurementId: 'G-SKPS2W99M1',
+	apiKey: 'AIzaSyC7-IEy7qqg97HhpF9-ettNXDzozfHt4bg',
+	authDomain: 'music-80470.firebaseapp.com',
+	projectId: 'music-80470',
+	storageBucket: 'music-80470.appspot.com',
+	appId: '1:143333674081:web:ff2de5a9ee308758d8ae20',
+	measurementId: 'G-SKPS2W99M1',
 };
 
 // Initialize Firebase
@@ -20,15 +20,19 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
+db.enablePersistence().catch((error) => {
+	console.log(`Firebase persistance error ${error.code}`)
+});
+
 const usersCollection = db.collection('users');
 const songsCollection = db.collection('songs');
 const commentsCollection = db.collection('comments');
 
 export {
-  auth,
-  db,
-  usersCollection,
-  songsCollection,
-  commentsCollection,
-  storage,
+	auth,
+	db,
+	usersCollection,
+	songsCollection,
+	commentsCollection,
+	storage,
 };
