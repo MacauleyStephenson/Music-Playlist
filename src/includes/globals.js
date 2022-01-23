84846fb3a25a69144fa7ec5306aaa946c3fd2b32
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-01-23 15:20:11 +0000

    progress with optimisations

--- src/includes/globals.js
+++ src/includes/globals.js
@@ -1,18 +1,19 @@
 import _ from 'lodash';
 
 export default {
-	install() {
+	install(app) {
 		const baseComponets = require.context(
 			'../components/base/', false, /[A-Za-z0-9-_,\s]+\.vue$/i,
 		);
 
 		baseComponets.keys().forEach((fileName) => {
-			// const componentConfig = baseComponets(fileName);
+			const componentConfig = baseComponets(fileName);
 			const componentName = _.upperFirst(_.camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, '')),
 			);
 
 
-			console.log(fileName, componentName);
+			//export default
+			app.component(`Base${componentName}`, componentConfig.default || componentConfig);
 		});
 	},
 };
