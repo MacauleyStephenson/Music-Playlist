108619dfc05f987ae8fc704b4adc32466a136bc9
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-01-31 18:50:36 +0000

    testing with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
-  root: true,
+	root: true,
 
-  env: {
-    node: true,
-  },
+	env: {
+		node: true,
+		jest: true,
+	},
 
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/airbnb',
+	],
 
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
 
-  rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-  },
+	rules: {
+		'no-console': 'off',
+		'no-debugger': 'off',
+	},
 
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+	overrides: [
+		{
+			files: [
+				'**/__tests__/*.{j,t}s?(x)',
+				'**/tests/unit/**/*.spec.{j,t}s?(x)',
+			],
+			env: {
+				jest: true,
+			},
+		},
+	],
 
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+	extends: [
+		'plugin:vue/essential',
+		'@vue/airbnb',
+	],
 };
