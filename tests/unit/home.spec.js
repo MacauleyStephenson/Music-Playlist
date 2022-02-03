1c5de7d19136cb36ea16e8bf85b9ede54bb108f7
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-02-03 15:44:29 +0000

    mocked firebase and mocking methods

--- tests/unit/home.spec.js
+++ tests/unit/home.spec.js
@@ -2,12 +2,16 @@ import { shallowMount } from "@vue/test-utils";
 import Home from '@/views/Home.vue';
 import songItem from '@/components/songItem.vue';
 
+jest.mock('@/includes/firebase', () => { });
+
 describe('Home.vue', () => {
 
 	test('readers list of songs', () => {
 		const songs = [
 			{}, {}, {},
 		];
+
+		Home.methods.getSongs = () => false;
 
 		const component = shallowMount(Home, {
 			data() {
